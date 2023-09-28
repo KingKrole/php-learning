eafa6601ef497f532f76f05565cc2c70fe8f954b
ColeE <hoopwalker@yahoo.com>
2023-09-27 18:55:30 -0700

    New array learning

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    
+</head>
+
+<body>
+    
+    <h1>Recommended Books</h1>
+
+    <?php
+        $Books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary",
+
+        ];
+    ?>
+
+    <ul>
+        <?php foreach ($books as $book) {
+            echo "<li>$book</li>";
+        }
+    </ul>
+
+
+</body>
+
+</html>
+cat
