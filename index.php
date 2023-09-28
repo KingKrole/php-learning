3e20247698b49ee848bb6df57ac2c7e778074f0e
ColeE <hoopwalker@yahoo.com>
2023-09-27 20:09:57 -0700

    New array work

--- index.php
+++ index.php
@@ -4,30 +4,39 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-    
+
 </head>
 
 <body>
-    
+
     <h1>Recommended Books</h1>
 
     <?php
-        $Books = [
-            "Do Androids Dream of Electric Sheep",
-            "The Langoliers",
-            "Hail Mary",
+    $books = [
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'purchaseUrl' => 'http:??example.com'
+        ],
 
-        ];
+        [
+            'name' => 'Do Androids Dream of Electric Sheep',
+            'author' => 'Philip k. Dick',
+            'purchaseUrl' =>  'http://examples.com'
+        ]
+    ];
+
     ?>
 
     <ul>
-        <?php foreach ($books as $book) {
-            echo "<li>$book</li>";
-        }
+        <?php foreach ($books as $book) : ?>
+            <li>
+                </= $book[0]; ?>
+            </li>
+        <?php endforeach; ?>
     </ul>
 
 
 </body>
 
 </html>
-cat
