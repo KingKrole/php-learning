b49d125d00b7845eba0fd96e8e45ba77f0a43c19
ColeE <hoopwalker@yahoo.com>
2023-09-28 17:57:27 -0700

    Learing funstions

--- index.php
+++ index.php
@@ -16,26 +16,48 @@
         [
             'name' => 'Project Hail Mary',
             'author' => 'Andy Weir',
+            'releaseYear' => 2021,
             'purchaseUrl' => 'http:??example.com'
         ],
 
         [
             'name' => 'Do Androids Dream of Electric Sheep',
             'author' => 'Philip k. Dick',
+            'releaseYear' => 1968,
             'purchaseUrl' =>  'http://examples.com'
+        ],
+
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
+            'purchaseUrl' => 'http://example.com'
         ]
     ];
+
+    function filterByAuthor($books, $author)
+    {
+        $filteredBooks = [];
+
+        foreach ($books as $book) {
+            if ($book['author'] === $author) {
+                $filteredBooks[] = $book;
+            }
+        }
+        return $filteredBooks;
+    }
 
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
+        <?php foreach (filterByAuthor($books, 'Philip k. Dick') as $book) : ?>
             <li>
-                </= $book[0]; ?>
+                <a href="<?= $book['purchaseUrl'] ?>">
+                    <?= $book['name']; ?> (<?= $book['releaseYear'] ?>)
+                </a>
             </li>
         <?php endforeach; ?>
     </ul>
-
 
 </body>
 
