af3aa0b361b56d64fd319ccf99753ab4581d1d77
ColeE <hoopwalker@yahoo.com>
2023-10-11 17:57:59 -0700

    Fixed errror for not displaying site.

--- router.php
+++ router.php
@@ -16,13 +16,15 @@ function routeToController($uri, $routes)
     if (array_key_exists($uri, $routes)) {
         require $routes[$uri];
     } else {
-        abort($code);
+        abort();
     }
 }
 
-function abort($code)
+function abort($code = 404)
 {
     http_response_code($code);
     require "views/{$code}.php";
     die();
 }
+
+routeToController($uri, $routes);
